d9a7ad500346240d94c014bfa8ee289403ab28f2
Christina Anglin <anglin.christina@gmail.com>
2018-01-15 00:45:27 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+  def self.current_user(session_hash)
+    @user = User.find(session_hash[:user_id])
+    @user
+  end
+
+  def self.is_logged_in?(session_hash)
+    @id = self.current_user(session_hash).id
+    if @id == session_hash[:user_id]
+      true
+    else
+      false
+    end
+  end
+
 end
